6b5011b0673bcedf58e1d74dd18a34ef0bcdead0
Laurent Pugin <lxpugin@gmail.com>
2021-03-05 08:09:32 +0100

    Add script to generate examples (WIP)
    * add scripts directory in config
    * add mei-examples.rb plugin that generates the scripts/examples.yaml file
    * add generate-examples.py that generates svg and mei snippets from examples.yaml
    * add mei templates that includes the examples in the html pages

---
+++ _plugins/mei-examples.rb
@@ -0,0 +1,55 @@
+
+module Jekyll
+  class MeiExamples < Generator
+    safe true
+    priority :high
+
+    def generate(site)
+
+      # Store an array of all examples we have in the book
+      examples = []
+      # The path to the inclue
+      meiExamples = site.source + "/_includes/"
+      svgExamples = site.source + "/images/"
+      
+      # The only collection we want to process
+      book = site.collections['book']
+      # Sort it by path
+      book.docs.sort_by(&:path).each do |doc|
+        # Remove the absolute part of the path
+        short_path = doc.path.gsub(book.directory, "")
+
+        if doc.data['examples']
+          # The path for an example is base on the directory of the file it is in.
+          example_path = get_example_path_for(short_path)
+          doc.data['examples'].each do |example|
+            # The SVG and the MEI snippet files
+            svgFile = example_path + example['name'] + ".svg"
+            meiFile = example_path + example['name'] + ".mei"
+            # Add them to the example list
+            example['mei-example-file'] = meiFile
+            example['svg-example-file'] = svgFile
+            examples.push(example)
+            # Here check if the file exists. If not, it means it still need to be generated with the 
+            # file need to be generated with ./scripts/generate-examples.py
+            # The flags set here are to inform the _include/mei template not to actually include them
+            puts svgExamples + svgFile
+            puts meiExamples + meiFile
+            example['svg-example-exists'] = true unless (!File.exists?(svgExamples + svgFile))
+            example['mei-example-exists'] = true unless (!File.exists?(meiExamples + meiFile))
+          end
+        end
+      end
+
+      # Generate an input file for the ./scripts/generate-examples.py
+      path = File.expand_path "scripts/examples.yaml", site.source
+      File.open(path, 'w') { |file| file.write(examples.to_yaml) }
+    end
+
+    def get_example_path_for(path)
+      example_path = "examples"
+      return example_path + path.gsub(/[^\/]*$/,"")
+    end
+
+  end
+end
---
+++ scripts/generate-examples.py
@@ -0,0 +1,116 @@
+# This script it expected to be run from ./bindings/python
+import argparse
+import json
+import os
+import re
+import sys
+import wget
+import xml.etree.ElementTree as ET
+import yaml
+
+from io import StringIO 
+
+# Add path for toolkit built in-place
+sys.path.append('.')
+import verovio
+
+ns = {'mei': 'http://www.music-encoding.org/ns/mei'}
+
+testOptions = {
+    'adjustPageHeight': True,
+    'breaks': 'none',
+    'pageHeight': 2970,
+    'pageWidth': 2100,
+    'header': 'none',
+    'footer': 'none',
+    'scale': 10,
+    'spacingStaff': 4,
+    'svgViewBox': True
+}
+
+def indent(elem, level=0):
+  i = "\n" + level*"  "
+  if len(elem):
+    if not elem.text or not elem.text.strip():
+      elem.text = i + "  "
+    if not elem.tail or not elem.tail.strip():
+      elem.tail = i
+    for elem in elem:
+      indent(elem, level+1)
+    if not elem.tail or not elem.tail.strip():
+      elem.tail = i
+  else:
+    if level and (not elem.tail or not elem.tail.strip()):
+      elem.tail = i
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    #parser.add_option("--clean", nargs='?', default=False)
+    args = parser.parse_args()
+
+    tk = verovio.toolkit()
+    # version of the toolkit
+    print(tk.getVersion())
+
+    # keep all the options to be able to reset them for each test
+    defaultOptions = json.loads(tk.getOptions(True))
+
+    with open(r'scripts/examples.yaml') as file:
+        examples = yaml.load(file, Loader=yaml.FullLoader)
+        for example in examples:
+            #if 'svg-example-exists' in example and example['svg-example-exists']:
+                #continue
+            #if 'mei-example-exists' in example and example['mei-example-exists']:
+                #continue
+
+            meiFile = wget.download(example['url'], out="./scripts/tmp")
+            print("\nFile successfully downloaded to .{}".format(meiFile))
+
+            options = {**defaultOptions, **testOptions}
+            # parse the MEI file
+            
+            tree = ET.parse(meiFile)
+
+            root = tree.getroot()
+            # try to get the extMeta tag and load the options if existing
+            meta = root.findtext(".//mei:meiHead/mei:extMeta", namespaces=ns)
+            if (meta != None and meta != ''):
+                print(meta)
+                metaOptions = json.loads(meta)
+                options = {**options, **metaOptions}
+
+            f = root.findall(".//mei:measure", namespaces=ns)
+            ET.register_namespace('', 'http://www.music-encoding.org/ns/mei')
+
+            meiSnippetStr = ""
+            for i in f:
+                indent(i)
+                xmlStr = ET.tostring(i, method='xml').decode()
+                if xmlStr:
+                    #xmlStr = re.sub(xmlStr, ' xmlns="http:\/\/www.music-encoding.org\/ns\/mei"', '')
+                    meiSnippetStr += xmlStr
+
+            meiSnippetFile = os.path.join("_includes", example['mei-example-file'])
+            meiSnippetDir = os.path.dirname(meiSnippetFile)
+            if not(os.path.isdir(meiSnippetDir)):
+                print(meiSnippetDir)
+                os.mkdir(os.path.join(meiSnippetDir))
+
+            with open(meiSnippetFile, 'w') as f:
+                f.write(meiSnippetStr)
+
+            tk.setOptions(json.dumps(options))
+            tk.loadFile(meiFile)
+
+            svgString = tk.renderToSVG(1)
+
+            # create the output directory if necessary
+            svgFile = os.path.join("images", example['svg-example-file'])
+            svgDir = os.path.dirname(svgFile)
+            if not(os.path.isdir(svgDir)):
+                print(svgDir)
+                os.mkdir(os.path.join(svgDir))
+
+            ET.ElementTree(ET.fromstring(svgString)).write(svgFile)
+
+            os.remove(meiFile) 
