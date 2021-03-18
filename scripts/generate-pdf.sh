bbbe279a400ea182872714ce92e97c997f1165e1
Laurent Pugin <lxpugin@gmail.com>
2021-03-18 13:21:23 +0100

    Change localhost to 127.0.0.1

--- scripts/generate-pdf.sh
+++ scripts/generate-pdf.sh
@@ -5,13 +5,13 @@
 wkhtmltopdf -g -s A4 --no-background  --print-media-type --outline --outline-depth 3  \
 -B 20 -R 20 -L 20 -T 20 \
 --minimum-font-size 18  \
-http://localhost:4000/full-chapters/01-introduction.html \
-http://localhost:4000/full-chapters/02-web-based-notation.html \
-http://localhost:4000/full-chapters/03-interactive-notation.html \
-http://localhost:4000/full-chapters/04-advanced-topics.html \
-http://localhost:4000/full-chapters/05-toolkit-reference.html \
-http://localhost:4000/full-chapters/06-installing-or-building-from-sources.html \
-http://localhost:4000/full-chapters/07-contributing.html \
+http://127.0.0.1:4000/full-chapters/01-introduction.html \
+http://127.0.0.1:4000/full-chapters/02-web-based-notation.html \
+http://127.0.0.1:4000/full-chapters/03-interactive-notation.html \
+http://127.0.0.1:4000/full-chapters/04-advanced-topics.html \
+http://127.0.0.1:4000/full-chapters/05-toolkit-reference.html \
+http://127.0.0.1:4000/full-chapters/06-installing-or-building-from-sources.html \
+http://127.0.0.1:4000/full-chapters/07-contributing.html \
 toc --xsl-style-sheet ./scripts/toc.xsl \
 scripts/verovio-reference-book.pdf
 
