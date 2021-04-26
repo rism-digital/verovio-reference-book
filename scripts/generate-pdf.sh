23a5e7a85fe93fc70b85d6af3d9fd4576159da3e
Laurent Pugin <lxpugin@gmail.com>
2021-04-26 17:50:59 +0200

    Change outline depth back to 3

--- scripts/generate-pdf.sh
+++ scripts/generate-pdf.sh
@@ -2,7 +2,7 @@
 # If the config.yml generate-full-chapters flag is on and the site is served locally, this script will
 # generate a PDF of the book
 
-wkhtmltopdf -g -s A4 --javascript-delay 500 --no-background  --print-media-type --outline --outline-depth 2  \
+wkhtmltopdf -g -s A4 --javascript-delay 500 --no-background  --print-media-type --outline --outline-depth 3  \
 -B 20 -R 20 -L 20 -T 20 \
 --minimum-font-size 18  \
 http://127.0.0.1:4000/pdf/index.html \
