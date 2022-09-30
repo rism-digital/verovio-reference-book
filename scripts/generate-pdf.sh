2f35db8776d17c4f7dcc98b403fcc04014463869
Laurent Pugin <lxpugin@gmail.com>
2022-09-30 17:31:13 +0200

    Regenerate examples and add --enable-local-file-access option

--- scripts/generate-pdf.sh
+++ scripts/generate-pdf.sh
@@ -6,6 +6,7 @@ wkhtmltopdf -s A4 --javascript-delay 500 --no-background  --print-media-type --o
 -B 20 -R 20 -L 20 -T 20 \
 --minimum-font-size 19  \
 --footer-center "– [page] –"  --footer-font-size 8 \
+--enable-local-file-access \
 http://127.0.0.1:4000/pdf/index.html \
 toc --xsl-style-sheet ./scripts/toc.xsl \
 scripts/verovio-reference-book.pdf
